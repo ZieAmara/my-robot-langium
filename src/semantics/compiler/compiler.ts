53102e11b62891e6901a38447e6aa046abdb73b1
Zié Amara <zie.traore18@inphb.ci>
2024-11-23 00:05:50 +0100

    config du chemin de la commande d'execusion de la compilateur

--- src/semantics/compiler/compiler.ts
+++ src/semantics/compiler/compiler.ts
@@ -2,10 +2,84 @@ import { Add, And, ArithmeticExpression, ArithmeticOperator, Backward, BooleanEx
 import { Visitor } from "../../language/visitorGenerator/visitor.js";
 
 export class CompilerVisitor implements Visitor {
+    private arduinoCode = `
+#include <PinChangeInt.h>
+#include <PinChangeIntConfig.h>
+#include <EEPROM.h>
+#define _NAMIKI_MOTOR	 //for Namiki 22CL-103501PG80:1
+#include <fuzzy_table.h>
+#include <PID_Beta6.h>
+#include <MotorWheel.h>
+#include <Omni4WD.h>
+
+//#include <fuzzy_table.h>
+//#include <PID_Beta6.h>
+
+/*
+
+            \                    /
+   wheel1   \                    /   wheel4
+   Left     \                    /   Right
+
+
+                              power switch
+
+            /                    \
+   wheel2   /                    \   wheel3
+   Right    /                    \   Left
+
+*/
+
+/*
+  irqISR(irq1,isr1);
+  MotorWheel wheel1(5,4,12,13,&irq1);
+
+  irqISR(irq2,isr2);
+  MotorWheel wheel2(6,7,14,15,&irq2);
+
+  irqISR(irq3,isr3);
+  MotorWheel wheel3(9,8,16,17,&irq3);
+
+  irqISR(irq4,isr4);
+  MotorWheel wheel4(10,11,18,19,&irq4);
+*/
+
+irqISR(irq1, isr1);
+MotorWheel wheel1(3, 2, 4, 5, &irq1);
+
+irqISR(irq2, isr2);
+MotorWheel wheel2(11, 12, 14, 15, &irq2);
+
+irqISR(irq3, isr3);
+MotorWheel wheel3(9, 8, 16, 17, &irq3);
+
+irqISR(irq4, isr4);
+MotorWheel wheel4(10, 7, 18, 19, &irq4);
+
+
+Omni4WD Omni(&wheel1, &wheel2, &wheel3, &wheel4);
+
+void setup() {
+  //TCCR0B=TCCR0B&0xf8|0x01;    // warning!! it will change millis()
+  TCCR1B = TCCR1B & 0xf8 | 0x01; // Pin9,Pin10 PWM 31250Hz
+  TCCR2B = TCCR2B & 0xf8 | 0x01; // Pin3,Pin11 PWM 31250Hz
+
+  Omni.PIDEnable(0.31, 0.01, 0, 10);
+}
+`;
     
-    visitProgram(node : Program) : any {}
+    visitProgram(node : Program) : any {
+        node.fonction.map(f => {
+            this.visitFonction(f as Fonction);
+        })
+        return this.arduinoCode
+    }
 	
-    visitFonction(node : Fonction) : any {}
+    visitFonction(node : Fonction) : any {
+        this.visitReturnType(node.returnType as ReturnType)
+        + node.name + "(" + node.parameter.map(p => this.visitParameter(p as Parameter)).join(",") + ") { \n" 
+        + node.body.map(s => this.visitStatement(s as Statement)).join("; \n") + "}";
+    }
 	
     visitReturnType(node : ReturnType) : any {}
 	
